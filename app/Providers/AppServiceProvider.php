9130a409135f927977d075566e1b4a08b7a5ddb8
zulfalsa <zulfa2805@gmail.com>
2025-12-04 10:08:23 +0700

    update service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,16 +3,20 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use App\Repositories\Interfaces\DocumentRepositoryInterface;
-use App\Repositories\EloquentRepositoryDocument;
 
-class RepositoryServiceProvider extends ServiceProvider
+class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     */
     public function register(): void
     {
-        $this->app->bind(DocumentRepositoryInterface::class, EloquentRepositoryDocument::class);
+        //
     }
 
+    /**
+     * Bootstrap any application services.
+     */
     public function boot(): void
     {
         //
